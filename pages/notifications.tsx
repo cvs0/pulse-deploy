8645d64c875ec2dfebc16641f3f2fd42d8e51f9e
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-03-27 13:17:26 -0600

    notif-fix

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import NotificationsFeed from "@/components/NotificationsFeed";
-import useCurrentUser from "@/hooks/useCurrentUser";
-import { NextPageContext } from "next";
-import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse, NextPageContext } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+  let req: NextApiRequest = context.req as NextApiRequest;
+  let res: NextApiResponse = context.res as NextApiResponse;
+  const session = await getServerSession(req, res, authOptions);;
 
   if (!session) {
     return {
